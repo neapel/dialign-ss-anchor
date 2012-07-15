d89b34b136f065a6223ac22c90ad033c649c2132
Pascal Schmitt <pascal.schmitt1@stud.uni-goettingen.de>
2012-07-15 03:42:06 +0200

    python 2.3 compat

--- ipssp2vformat.py
+++ ipssp2vformat.py
@@ -14,11 +14,11 @@ import sys
 # ipssp: L=coil, H=alpha, E=beta
 
 translate = {
-	'L': [1.0, 0.0, 0.0],
-	'H': [0.0, 1.0, 0.0],
-	'E': [0.0, 0.0, 1.0]
+	'L': (1.0, 0.0, 0.0),
+	'H': (0.0, 1.0, 0.0),
+	'E': (0.0, 0.0, 1.0)
 }
 
 _, seq, struct = list(sys.stdin)
-for i, (r, s) in enumerate(zip(seq.strip(), struct.strip()), 1):
-	print '{0:4d} {1} {2}  {3:6.3f} {4:6.3f} {5:6.3f}'.format(i, r, s, *translate[s])
+for i, (r, s) in enumerate(zip(seq.strip(), struct.strip())):
+	print '%4d %s %s  %6.3f %6.3f %6.3f' % ((i + 1, r, s) + translate[s])
